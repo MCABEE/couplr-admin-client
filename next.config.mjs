0e7a514d7a45ffa3975bdf7b81688338058e6bfa
Ijas Moopan <ijasmoopan46@gmail.com>
2025-04-07 19:54:38 +0530

    feat(refactor-1): setup docker

--- next.config.mjs
+++ next.config.mjs
@@ -1,30 +1,29 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
+import dotenv from "dotenv";
+import { PHASE_PRODUCTION_BUILD } from "next/constants.js";
 
-// export default nextConfig;
-
-// next.config.js
-// next.config.mjs
-
-import webpack from 'webpack'
-import dotenv from 'dotenv'
-
-dotenv.config()
+const envFile = `env/.env.${process.env.APP_ENV || "local"}`;
+dotenv.config({ path: envFile });
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config) => {
-    config.plugins.push(
-    //   new webpack.EnvironmentPlugin(process.env)
-    new webpack.EnvironmentPlugin([
-        'NEXT_PUBLIC_LOGIN_URL',
-        'NEXT_PUBLIC_API_URL'
-      ])
-    )
-    return config
-  }
-}
+const nextConfig = (phase) => {
+  const isProd = phase === PHASE_PRODUCTION_BUILD;
 
-export default nextConfig
+  return {
+    reactStrictMode: true,
+    compress: true,
+    env: {
+      NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+      NEXT_PUBLIC_LOGIN_URL: process.env.NEXT_PUBLIC_LOGIN_URL,
+    },
+    ...(isProd && {
+      eslint: {
+        ignoreDuringBuilds: true,
+      },
+      typescript: {
+        ignoreBuildErrors: true,
+      },
+    }),
+  };
+};
 
-
+export default nextConfig;
